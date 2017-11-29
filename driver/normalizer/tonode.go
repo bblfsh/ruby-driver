74223b02a3899603094f567195668e66ba1e3904
Juanjo Alvarez <juanjo@sourced.tech>
2017-11-29 13:56:26 +0100

    Fills ToNode, update integration tests.
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- driver/normalizer/tonode.go
+++ driver/normalizer/tonode.go
@@ -6,4 +6,24 @@ import "gopkg.in/bblfsh/sdk.v1/uast"
 // into a UAST (`uast.Node`).
 //
 // https://godoc.org/gopkg.in/bblfsh/sdk.v1/uast#ObjectToNode
-var ToNode = &uast.ObjectToNode{}
+var ToNode = &uast.ObjectToNode{
+	InternalTypeKey: "type",
+	LineKey: "start_line",
+	EndLineKey: "end_line",
+	ColumnKey: "start_col",
+	EndColumnKey: "end_col",
+
+	TokenKeys: map[string]bool {
+		"name": true,
+		"token": true,
+		"selector": true,
+		"target": true,
+	},
+	Modifier: func(n map[string] interface{}) error {
+		// Native parser uses a [) interval for columns, so add 1 to start_col
+		if col, ok := n["start_col"].(float64); ok {
+			n["start_col"] = col + 1
+		}
+		return nil
+	},
+}
